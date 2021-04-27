50d2649a5ffb5a6d070863075171145c27fc3fe3
Julie VALBERT <julie.valbert@ens.uvsq.fr>
2021-04-27 21:44:41 +0200

    suppression fichier inutile

--- personnage_deplacement.py
+++
@@ -1,8 +0,0 @@
-import Terrain.py
-
-def personnage(event):
-    canvas.create_oval(RAPORT_CASE_C/3, RAPORT_CASE_R/3, 2*RAPORT_CASE_C/3, 2*RAPORT_CASE_R/3, fill="red")
-
-
-
-canvas.bind('<Button-1>', personnage)
